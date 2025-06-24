124a027d650c7c77779b3c929f52bd0e7a3225e7
Ankit Rawat <155627104+ankitrawat579@users.noreply.github.com>
2025-06-24 16:25:40 +0530

    delete

--- force-app/main/default/lwc/message/message.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-import EmployeePortalHighlight from '@salesforce/label/c.Employee_Portal_Highlight'
-export default class Message extends LightningElement {
-    label = {
-        EmployeePortalHighlight, 
-    };
-}
